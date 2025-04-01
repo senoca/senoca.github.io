1e3810c608ed3a3c3a524aea7e21509925404ea8
senoca <snc.gen@protonmail.com>
2025-04-01 18:39:59 +0200

    new

---
+++ deploy.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    char buf[100];
+    printf("Deploying updates in blog...\n");
+    if (argc < 2)
+    {
+        printf("You must specify the commit comment!\n");
+        printf("The blog was not updated\n");
+        return 1;
+    }
+
+    snprintf(buf, sizeof(buf), "hugo --minify -d docs");
+    if (system(buf) == -1) {
+       printf("Error: Static data could not be generated\n");
+    }
+
+    snprintf(buf, sizeof(buf), "git add .");
+    if (system(buf) == -1)
+    {
+       printf("Error: Git could not add data\n");
+    }
+
+    snprintf(buf, sizeof(buf), "git commit -m \"%s\"", argv[1]);
+    if (system(buf) == -1) {
+       printf("Error: Git could not commit\n");
+    }
+
+    snprintf(buf, sizeof(buf), "git push");
+    if (system(buf) == -1) {
+       printf("Error: Git could not push\n");
+    }
+
+    printf("Blog updated");
+   return 0;
+}

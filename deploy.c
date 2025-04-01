3a8a6707e949adc5d265a8c243d26f3580620df3
senoca <snc.gen@protonmail.com>
2025-04-01 18:49:32 +0200

    new autodeploy

--- deploy.c
+++ deploy.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -28,7 +29,22 @@ int main(int argc, char *argv[])
        printf("Error: Git could not add data\n");
     }
 
-    snprintf(buf, sizeof(buf), "git commit -m \"%s\"", argv[1]);
+    char comment[100];
+
+    for (int i = 1; i < argc; i++) { // Start from argv[1]
+        if (strlen(comment) + strlen(argv[i]) + 2 >= sizeof(comment)) {
+            printf("Error: comment size exceeded\n");
+            return 1;
+        }
+
+        if (i > 1) {
+            strncat(comment, " ", sizeof(comment) - strlen(comment) - 1); // Add space separator
+        }
+
+        strncat(comment, argv[i], sizeof(comment) - strlen(comment) - 1);
+    }
+
+    snprintf(buf, sizeof(buf), "git commit -m \"%s\"", comment);
     if (system(buf) == -1) {
        printf("Error: Git could not commit\n");
     }
